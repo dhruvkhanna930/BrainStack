75341417f5013cf3ef0ecc87f08c53e310166c1e
“dhruvkhanna930” <dhruvkhanna930@gmail.com>
2024-01-22 15:46:09 +0530

    fexed feed scrolling

--- brainshare/urls.py
+++ brainshare/urls.py
@@ -4,7 +4,6 @@ from .views import *
 
 urlpatterns = [
     path('', home, name = 'bs-home'),
-    # path('2/', home1, name = 'bs-home1'),
     path('download/<uid>', download),
     path('handle/', HandleFileUpload.as_view())
 ]
