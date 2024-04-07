2a8628b39eda9f616e64ed5171ede2f3a34dfa25
“dhruvkhanna930” <dhruvkhanna930@gmail.com>
2024-04-08 04:45:36 +0530

    Quick fixes

--- build.sh
+++ build.sh
@@ -4,6 +4,7 @@
 
 echo "Building the project ..."
 python3.9 -m pip install -r requirements.txt
+python3.9 -m pip install django
 
 echo "Make migration..."
 python3.9 manage.py makemigrations --noinput
