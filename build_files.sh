ba0113d6889a98e6656980ec26857334896ed9eb
“dhruvkhanna930” <dhruvkhanna930@gmail.com>
2024-04-08 00:30:08 +0530

    Changed venv 4

--- build_files.sh
+++ build_files.sh
@@ -1,5 +1,5 @@
 echo " BUILD START"
 # build_files.sh
-pip install -r requirements.txt
-python3.9 manage.py collectstatic
+pip3 install -r requirements.txt
+python3.10 manage.py collectstatic
 echo " BUILD END"
